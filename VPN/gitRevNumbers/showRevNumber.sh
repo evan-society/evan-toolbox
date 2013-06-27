a31f41332ea38bd3b277c4d390d241fb70e4e93f
Peter Houska <peter.houska@gmail.com>
2013-06-27 17:17:42 +0200

    moved git-svn-revision workaround script folder to a more appropriate location, since it is not part of 'Evan' added more git ignore rules (mainly related to Qt)

--- VPN/gitRevNumbers/showRevNumber.sh
+++ VPN/gitRevNumbers/showRevNumber.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # http://oderwat.de/2011/05/22/git-mit-revisions-nummern-ahnlich-wie-bei-subversion-nutzen/
 
-# we start from SVN revision 1.63
+# we start from SVN revision 1663
+# the displayed version string reads 1.63
 BASE_SVN_REV="1.63"
 
-#GIT_REV="1"
+
 GIT_REV=`git rev-list HEAD | wc -l`
 
-echo "GIT_REV=${GIT_REV}"
+# has unnecessary whitespaces in string/output
+#echo "GIT_REV=${GIT_REV}"
+
 # http://linux.byexamples.com/archives/42/command-line-calculator-bc/
 # scale means the precision of floating point, i.e. how many digit after the point
 #echo "scale=3; ${BASE_SVN_REV} + ${GIT_REV}/1000.0 " | bc
@@ -19,6 +22,6 @@ echo "GIT_REV=${GIT_REV}"
 # direct trim
 echo "${BASE_SVN_REV}.${GIT_REV}" | tr -d ' '
 
-# and some usage: http://oderwat.de/2011/05/22/git-mit-revisions-nummern-ahnlich-wie-bei-subversion-nutzen/
+# and some usage:
 # mapping back to GIT IDs
 # git show-rev-number 1234
