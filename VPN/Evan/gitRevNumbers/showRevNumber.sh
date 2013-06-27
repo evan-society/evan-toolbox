fcab860f297f646851503fd04f32fb07bb9f8ac0
Peter Houska <peter.houska@gmail.com>
2013-06-27 16:21:49 +0200

    starting to add svn-revision workaround in git for the About.xml file

---
+++ VPN/Evan/gitRevNumbers/showRevNumber.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# http://oderwat.de/2011/05/22/git-mit-revisions-nummern-ahnlich-wie-bei-subversion-nutzen/
+
+# we start from SVN revision 1.63
+BASE_SVN_REV="1.63"
+
+#GIT_REV="1"
+GIT_REV=`git rev-list HEAD | wc -l`
+
+echo "GIT_REV=${GIT_REV}"
+# http://linux.byexamples.com/archives/42/command-line-calculator-bc/
+# scale means the precision of floating point, i.e. how many digit after the point
+#echo "scale=3; ${BASE_SVN_REV} + ${GIT_REV}/1000.0 " | bc
+
+# bc command to trim GIT_REV string
+#GIT_REV_STR=`echo "${GIT_REV}" | bc`
+#echo "${BASE_SVN_REV}.${GIT_REV_STR}"
+
+# direct trim
+echo "${BASE_SVN_REV}.${GIT_REV}" | tr -d ' '
+
+# and some usage: http://oderwat.de/2011/05/22/git-mit-revisions-nummern-ahnlich-wie-bei-subversion-nutzen/
+# mapping back to GIT IDs
+# git show-rev-number 1234
