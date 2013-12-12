5eb7cfc74e6d762fcfe4202c0582b73a21f22bdb
pjh <huskytime@hotmail.com>
2013-12-12 22:35:52 +0100

    Refresh.bat for ui files adjusted to not do more than the respective SVN version

--- VPN/Evan/MainWindow/Designer/Refresh.bat
+++ VPN/Evan/MainWindow/Designer/Refresh.bat
@@ -1,1 +1,6 @@
 uic MainWindow.ui -o ui_MainWindow.h
+
+REM only the single line above was meant to be executed in the latest SVN revision...
+REM uic license.ui -o ui_license.h
+REM uic register.ui -o ui_register.h
+REM uic registerInstructions.ui -o ui_regInstruct.h
